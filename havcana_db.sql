ba71edb4e0dab41d51147925932ae795cf039318
Marcelo Torres <kevin.torres85@outlook.es>
2025-03-21 02:44:59 -0500

    db

--- havcana_db.sql
+++ havcana_db.sql
@@ -1,16 +1,16 @@
-
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-03-2025 a las 07:16:31
+-- Tiempo de generación: 21-03-2025 a las 08:44:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,13 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `comentarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) DEFAULT 'Anónimo',
   `tipo_comentario` varchar(50) NOT NULL,
   `comentario` text NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `nombre`, `tipo_comentario`, `comentario`, `fecha`) VALUES
+(4, 'Anónimo', 'sugerencia', 'prueba 15', '2025-03-21 06:39:13'),
+(6, 'Marcelo', 'consulta', 'Consulta de compra', '2025-03-21 06:41:02'),
+(7, 'Anónimo', 'producto', 'prueba 16', '2025-03-21 06:52:23');
 
 -- --------------------------------------------------------
 
@@ -50,16 +58,8 @@ CREATE TABLE `pedidos` (
   `subtotal` decimal(10,2) NOT NULL,
   `envio` decimal(10,2) NOT NULL,
   `total` decimal(10,2) NOT NULL,
-  `estado` varchar(20) DEFAULT 'pendiente' CHECK (`estado` in ('pendiente','completado','realizado','eliminado'))
+  `estado` varchar(20) DEFAULT 'pendiente' CHECK (`estado` in ('pendiente','completado','eliminado'))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `pedidos`
---
-
-INSERT INTO `pedidos` (`id`, `numero_pedido`, `usuario_id`, `fecha_pedido`, `subtotal`, `envio`, `total`, `estado`) VALUES
-(1, '#19032025001', 2, '2025-03-19 04:31:08', 14.98, 5.00, 19.98, 'realizado'),
-
 
 -- --------------------------------------------------------
 
@@ -81,11 +81,11 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `maridaje`, `precio`, `imagen`) VALUES
-(1, 'Vino de Pepiche', 'Sabor exótico y refrescante\r\n¡Recuerda! Por compra de 12 unidades (1 caja), el precio por unidad baja a $7,75 ', 'Marida bien con mariscos y ensaladas', 9.99, '../anexos/imagenes/pepiche.jpg'),
-(2, 'Vino de Manzana', 'Dulce y aromático con notas frutales\r\n¡Recuerda! Por compra de 12 unidades (1 caja), el precio por unidad baja a $3,85 ', 'Ideal para postres y quesos suaves', 4.99, '../anexos/imagenes/manzana.jpg'),
-(3, 'Vino de Uva Caimorona', 'Sabor intenso y aterciopelado\r\n¡Recuerda! Por compra de 12 unidades (1 caja), el precio por unidad baja a $7,75 ', 'Perfecto para carnes rojas y quesos curados', 9.99, '../anexos/imagenes/uvacaimorona.jpg'),
-(4, 'Vino de Uvilla', 'Equilibrado y ligeramente ácido/dulce\r\n¡Recuerda! Por compra de 12 unidades (1 caja), el precio por unidad baja a $7,75 ', 'Acompaña bien pescados y comidas ligeras', 9.99, '../anexos/imagenes/uvilla.jpg'),
-(5, 'Vino Flor y Uva', 'Delicado bouquet floral y frutal\r\n¡Recuerda! Por compra de 12 unidades (1 caja), el precio por unidad baja a $7,75 ', 'Marida con platos frescos y postres', 9.99, '../anexos/imagenes/floryuva.jpg');
+(1, 'Vino de Pepiche', 'Sabor exótico y refrescante', 'Marida bien con mariscos y ensaladas', 9.99, '../anexos/imagenes/Recurso 11.png'),
+(2, 'Vino de Manzana', 'Dulce y aromático con notas frutales', 'Ideal para postres y quesos suaves', 4.99, '../anexos/imagenes/Recurso 10.png'),
+(3, 'Vino de Uva Caimorona', 'Sabor intenso y aterciopelado', 'Perfecto para carnes rojas y quesos curados', 9.99, '../anexos/imagenes/Recurso 8.png'),
+(4, 'Vino de Flor Jamaica y Uva', 'Delicado bouquet floral y frutal', 'Marida con platos frescos y postres', 9.99, '../anexos/imagenes/Recurso 9.png'),
+(5, 'Vino Uvilla', 'Equilibrado y ligeramente ácido', 'Acompaña bien pescados y comidas ligeras', 9.99, '../anexos/imagenes/uvilla.jpg');
 
 -- --------------------------------------------------------
 
@@ -111,11 +111,19 @@ INSERT INTO `usuarios` (`id`, `nombre_completo`, `correo`, `usuario`, `contrasen
 (2, 'Marcelo Torres', 'kevin.torres85@outlook.es', 'MarceloKP13', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 0),
 (3, 'Jhoel Mariño', 'jhoel@admin.com', 'Jhoel', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1),
 (4, 'Marcelo', 'marce@admin.com', 'Marce', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1),
-(5, 'Prueba85', 'prueba85@gmail.com', 'Prueba85', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1);
+(5, 'Prueba85', 'prueba85@gmail.com', 'Prueba85', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1),
+(6, 'Lenaida Pinza', 'lenaidapinza84@gmail.com', 'Lenny', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1),
+(7, 'Kathe Reinoso', 'kathe06@admin.com', 'Kathe', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 1);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `pedidos`
@@ -146,13 +154,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
@@ -164,7 +172,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
