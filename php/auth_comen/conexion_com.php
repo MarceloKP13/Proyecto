8b6f9a54d3c943eaed82dce337762c6495c00470
MarceloKP13 <kevin.torres85@outlook.es>
2025-03-24 15:27:13 -0500

    conexion_com.php

--- php/auth_comen/conexion_com.php
+++ php/auth_comen/conexion_com.php
@@ -1,13 +1,10 @@
 <?php
-$host = 'sql108.epizy.com';
-$dbname = 'if0_38584302_havcana_db';
-$username = 'if0_38584302';
-$password = 'u5x0DWHfc6l';
+$conexion = new PDO("mysql:host=sql305.infinityfree.com;dbname=if0_38584302_havcana_db", "if0_38584302", "u5x0DWHfc6l");
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
+
